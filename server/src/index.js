16f502946a2b341b739e69de2a46afb2d213a85d
ShubhamJadhav12 <118345908+ShubhamJadhav12@users.noreply.github.com>
2025-03-06 01:04:33 +0530

    All my project

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://demo123:demo123@clustersnj.na83z.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSNJ",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
